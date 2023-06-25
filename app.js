f1568b85d3ad52cb3d9cb74b33ac804cbb473377
Youra931 <dbfk931@naver.com>
2023-06-26 06:15:45 +0900

    mod

--- app.js
+++ app.js
@@ -186,18 +186,10 @@ function deleteAllBtnClick(event) {
     }
 }
 
-function inputEnter(event) {
-    event.preventDefault();
-    if(event.keyCode == 13) {
-        addBtnClick(event);
-    }
-}
-
 todoAddBtn.addEventListener("click", addBtnClick);
 completeBtn.addEventListener("click", completeListPaint);
 incompleteBtn.addEventListener("click", completeListPaint);
 deleteAllBtn.addEventListener("click", deleteAllBtnClick);
-document.getElementById("todoText").addEventListener("keyup", inputEnter);
 
 function logoutBtnClick(event) {
     if(confirm("로그아웃 하시겠습니까?")) {
